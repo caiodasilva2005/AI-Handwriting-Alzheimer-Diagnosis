343821908be218e76bbd5b9b8ee22459b77a8013
Caio DaSilva <dasilva.cai@northeastern.edu>
2026-05-22 11:21:19 -0400

    training a resnet model

--- data.py
+++ data.py
@@ -2,10 +2,12 @@
 import os
 from os import path
 from enum import Enum
-
 import pandas as pd
 import kagglehub
 import shutil
+import pandas as pd
+from torch.utils.data import Dataset
+from torchvision.io import decode_image
 
 class SampleType(Enum):
     RGB_ON_PAPER = "rgb_on_paper"
@@ -59,3 +61,23 @@ class DarwinDownloader:
         print(f"Saving annotations to {annotations_path}")
         pd.DataFrame(rows, columns=["filepath", "label"]).to_csv(annotations_path, index=False)
         return (annotations_path, image_dir_path)
+
+class HandwritingAlzheimerDataset(Dataset):
+    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
+        self.img_labels = pd.read_csv(annotations_file)
+        self.img_dir = img_dir
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        return len(self.img_labels)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+        image = decode_image(img_path)
+        label = self.img_labels.iloc[idx, 1]
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            label = self.target_transform(label)
+        return image, label
---
+++ train.py
@@ -0,0 +1,61 @@
+
+from model import CNN
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torchvision.transforms import v2
+from data import DarwinDownloader, HandwritingAlzheimerDataset, SampleType
+
+NUM_ITERATIONS = 2
+
+transform = v2.Compose([
+    v2.ToImage(),
+    v2.Resize((224, 224)),
+    v2.ToDtype(torch.float32, scale=True),
+    # Normalized to values corresponding to the ImageNet dataset
+    v2.Normalize(mean=(0.485, 0.456, 0.406),
+                 std=(0.229, 0.224, 0.225))])
+
+net = CNN()
+lossFunction = nn.CrossEntropyLoss()
+
+# Learning rate: 1e-4 (small for fine-tuning pretrained weights; a larger LR would wipe out the ImageNet features)
+# Momentum: 0.9 (allows for the optimizer to accelerate in the correct direction of the current step's gradient)
+optimizer = optim.SGD(net.parameters(), lr=1e-4, momentum=0.9)
+
+downloader = DarwinDownloader()
+annotations, image_dir = downloader.generateAnnotations(SampleType.RGB_ON_PAPER, 2)
+trainset = HandwritingAlzheimerDataset(annotations_file=annotations, img_dir=image_dir, transform=transform)
+trainloader = DataLoader(trainset, batch_size=32, shuffle=True)
+
+class Trainer:
+    def __init__(self, model, loss_function, optimizer, trainloader):
+        self.model = model
+        self.loss_function = loss_function
+        self.optimizer = optimizer
+        self.trainloader = trainloader
+
+    def train(self, num_iterations):
+        for _ in range(num_iterations):  
+            for data in self.trainloader:
+                inputs, labels = data
+
+                # zero the parameter gradients
+                self.optimizer.zero_grad()
+
+                # forward + backward + optimize
+                outputs = self.model(inputs)
+                loss = self.loss_function(outputs, labels)
+                loss.backward()
+
+                self.optimizer.step()
+
+    def save_model(self, path):
+        torch.save(self.model.state_dict(), path)
+
+trainer = Trainer(net, lossFunction, optimizer, trainloader)
+trainer.train(NUM_ITERATIONS)
+trainer.save_model("model.pth")
+
+print('Finished Training')
